e6c00b0e734af77a41eaa13cff4ca24ef23a738b
Shiv Shankar <ps431352@gmail.com>
2024-10-18 01:55:09 +0530

    Added vercel.json and Readme file

---
+++ README.md
@@ -0,0 +1,16 @@
+# Rule Engine with AST
+
+## Overview
+This is a simple rule engine application backend that uses Abstract Syntax Trees (AST) to evaluate user rules based on criteria like age, salary, and experience.
+
+### Features
+- Create rules using rule strings.
+- Combine multiple rules into a single AST.
+- Evaluate rules against user data.
+
+## Setup
+
+1. Clone the repository.
+2. Install the dependencies:
+   ```bash
+   npm install
--- app.js
+++ app.js
@@ -1,6 +1,9 @@
 import express from 'express';
+import bodyParser from 'body-parser';
 import dotenv from 'dotenv'
 import cors from 'cors';
+import morgan from 'morgan';
+import ruleRoutes from './routes/ruleRoutes.js';
 import { connectDB } from './config/db.js';
 dotenv.config();
 
@@ -9,6 +12,7 @@ const PORT = process.env.PORT || 5000;
 
 // Middleware
 app.use(bodyParser.json());
+app.use(morgan('dev'));
 app.use(cors());
 
 // Connect to the database
@@ -20,7 +24,7 @@ app.get('/', (req, res) => {
 
 
 // Routes
-
+app.use('/api/rules', ruleRoutes);
 
 // Start the server
 app.listen(PORT, () => {
